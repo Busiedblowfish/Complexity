5f3d31dccc3b980201c8c46fb59846315559833f
Busiedblowfish <oobey1@aol.com>
2016-10-30 05:29:13 -0400

    Rockyou8.txt removed, bool func() added

--- complexity.cpp
+++ complexity.cpp
@@ -5,18 +5,17 @@
 #include <fstream>
 
 
-#define S_C " `~!@#$%^&*()-_+={}[]\\|:;\"\'<>,.?/"
+#define SC " `~!@#$%^&*()-_+={}[]\\|:;\"\'<>,.?/"
 #define MAX_SCORE 5
 #define MIN_SCORE 1
 
 /* Function prototype */
-int pwdLength(const std::string&);              //Return the length of the password
-bool dictionaryCheck(const std::string&);       //Return 1 for true or 0 for false if the password is in the dictionary[dictionary.txt]
-int atleast1Letter (const std::string&);        //Return the number of letter[small] in the password
-int atleast1CL(const std::string&);             //Return the number of capital letter in the password
-int atleast1Num(const std::string&);            //Return the number of numbers in the password
-int atleast1SC(const std::string&);             //Return the number of special characters in the password
-bool rockyouCheck(const std::string&);          //Return 1 for true or 0 for false if the password is in the rockyou8.txt
+int pwdLength(const std::string&);               //Return the length of the password
+bool inDictionary(const std::string&);           //Return 1 for true or 0 for false if the password is in the dictionary[dictionary.txt]
+bool atleast1Letter (const std::string&);        //Return the number of letter[small] in the password
+bool atleast1CL(const std::string&);             //Return the number of capital letter in the password
+bool atleast1Num(const std::string&);            //Return the number of numbers in the password
+bool atleast1SC(const std::string&);             //Return the number of special characters in the password
 
 
 /*
@@ -33,8 +32,7 @@ bool rockyouCheck(const std::string&);          //Return 1 for true or 0 for fal
     3. At least 1 letter [a-z]                          [1]
     4. At least 1 capital letter [A-Z]                  [1]
     5. At least 1 number [0-9]                          [1]
-    6. Not in rockyou.txt                               [1]
-    7. At least 1 special character                     [1]
+    6. At least 1 special character                     [1]
 
     A combination of all this criteria will yield the maximum strength of 5
 */
@@ -58,10 +56,21 @@ int main()
         if (input == "q")
         {
             break;
-            exit(EXIT_SUCCESS);
+            exit(0);
         }
         else if(input[0] == '\0')
             continue;
+        else if (pwdLength(input) < 8 && (inDictionary(input) == 1 || atleast1CL(input) == 1 || atleast1Letter(input) == 1 || atleast1Num(input) == 1 || atleast1SC(input) == 1))
+            score = MIN_SCORE;
+        else if (pwdLength(input) >= 8 && inDictionary(input) == 0 || (atleast1CL(input) == 1 || atleast1Letter(input) == 1 || atleast1Num(input) == 1 || atleast1SC(input) == 1))
+            score = MIN_SCORE + 1;
+        else if (pwdLength(input) >= 8 && inDictionary(input) == 0 && (atleast1CL(input) == 1 && atleast1Letter(input) == 1 || atleast1Num(input) == 1 || atleast1SC(input) == 1))
+            score = MIN_SCORE + 2;
+        else if (pwdLength(input) >= 8 && inDictionary(input) == 0 && (atleast1CL(input) == 1 && atleast1Letter(input) == 1 && atleast1Num(input) == 1 || atleast1SC(input) == 1))
+            score = MIN_SCORE + 3;
+        else
+            score = MAX_SCORE;
+
         //std::cout << "\nUser input: " << input;  //Verify the input
 /*
                 CRITERIA                              STRENGTH
@@ -71,31 +80,20 @@ int main()
     3. At least 1 letter [a-z]                          [1]
     4. At least 1 capital letter [A-Z]                  [1]
     5. At least 1 number [0-9]                          [1]
-    6. Not in rockyou.txt                               [1]
-    7. At least 1 special character                     [1]
+    6. At least 1 special character                     [1]
 
     A combination of all this criteria will yield the maximum strength of 5
 
 */
-        else if (pwdLength(input) < 8 || (dictionaryCheck(input) == 1) || (atleast1CL(input) >= 1) || (atleast1Letter(input) >= 1))
-            score = MIN_SCORE;
-        else if (pwdLength(input) >= 8 && dictionaryCheck(input) == 0 && atleast1CL(input) >= 1 || atleast1Letter(input) >= 1)
-            score = MIN_SCORE + 1;
-        else if (pwdLength(input) >= 8 && dictionaryCheck(input) == 0 && (atleast1CL(input) >= 1 || atleast1Letter(input) >= 1 && atleast1Num(input) >= 1))
-            score = MIN_SCORE + 2;
-        else if (pwdLength(input) >= 8 && dictionaryCheck(input) == 0 && (atleast1CL(input) >= 1 && atleast1Letter(input) >= 1 && atleast1Num(input) >= 1))
-            score = MIN_SCORE + 3;
-        else if (pwdLength(input) >= 8 && dictionaryCheck(input) == 0 && (atleast1CL(input) >= 1 && atleast1Letter(input) >= 1 && atleast1Num(input) >= 1) && rockyouCheck(input) == 0 && atleast1SC(input))
-            score = MAX_SCORE;
-        else
-            score = 3;
-        std::cout << "Password strength: " << score;
+
+        std::cout << "Password strength: " <<  score << "\n";
 /*
         std::cout << "\nThere are " << pwdLength(input) <<" characters";
-        std::cout << "\nIs the password in the dictionary? " << dictionaryCheck(input) << "\n";
-        std::cout << "\nNumber of letters: " << atleast1Letter(input) << "\n";
-        std::cout << "\nNumber of \'numbers\': " << atleast1Num(input) << "\n";
-        std::cout << "\nNumber of special characters: " << atleast1SC(input) << "\n";
+        std::cout << "\nIs the password in the dictionary? " << inDictionary(input) << "\n";
+        std::cout << "\nAt least 1 small letter: " << atleast1Letter(input) << "\n";
+        std::cout << "\nAt least 1 capital letter: " << atleast1CL(input) << "\n";
+        std::cout << "\nAt least 1 number: " << atleast1Num(input) << "\n";
+        std::cout << "\nAt least 1 special character: " << atleast1SC(input) << "\n";
 */
     }
     return 0;
@@ -111,7 +109,7 @@ int pwdLength (const std::string &pwd)
 
 
 /* Check if the password is in the dictionary */
-bool dictionaryCheck(const std::string &pwd)
+bool inDictionary(const std::string &pwd)
 {
     /* dictionary.txt from 'https://github.com/dwyl/english-words/blob/master/words3.txt' */
     std::string word;
@@ -125,12 +123,10 @@ bool dictionaryCheck(const std::string &pwd)
             if(word == pwd.c_str())    //The password is in the list
             {
                 found = true;
-                std::cout <<"\nThis is the word: " << word;
+                //std::cout <<"\nThis is the word: " << word;
                 //std::cout << "\nFound inside the loop: " << found;
-                break;
+                //break;
             }
-            else
-                found = false;
         }
         file.close();
     }
@@ -143,7 +139,7 @@ bool dictionaryCheck(const std::string &pwd)
 
 
 /* Check for at least 1 letter [a-z] */
-int atleast1Letter (const std::string &pwd)
+bool atleast1Letter (const std::string &pwd)
 {
     bool letterFound;
     bool found;
@@ -161,16 +157,14 @@ int atleast1Letter (const std::string &pwd)
             letterFound = false;
     }
     //std::cout << "\nNumber of letters found: " << j;
-    //std::cout << "\nLetter found? " << found;
     return j;
 }
 
 
 /* check for at least 1 capital letter [A-Z] */
-int atleast1CL (const std::string &pwd)
+bool atleast1CL (const std::string &pwd)
 {
-    bool clFound;
-    bool found;
+    bool clFound =  false;
     int length = pwdLength(pwd.c_str());
     int j = 0;  //count the number of capital letters
     for(int i = 0;  i < length;  i++)
@@ -178,22 +172,17 @@ int atleast1CL (const std::string &pwd)
         if(pwd.c_str()[i] >= 'A' && pwd.c_str()[i] <= 'Z')
         {
             clFound = true;
-            found = clFound;
             j = j + 1;
         }
-        else
-            clFound = false;
     }
     //std::cout << "\nNumber of capital letters found: " << j;
-    //std::cout << "\nCapital letter found? " << found;
-    return j;
+    return clFound;
 }
 
 /* check for at least 1 special character [] */
-int atleast1Num (const std::string &pwd)
+bool atleast1Num (const std::string &pwd)
 {
     bool numFound;
-    bool found;
     int length = pwdLength(pwd.c_str());
     int j = 0;  //count the number of capital letters
     for(int i = 0;  i < length;  i++)
@@ -201,72 +190,31 @@ int atleast1Num (const std::string &pwd)
         if((pwd.c_str()[i] - '0')  >= 0 && (pwd.c_str()[i] - '0' <= 9))
         {
             numFound = true;
-            found = numFound;
             j = j + 1;
         }
-        else
-            numFound = false;
     }
     //std::cout << "\nNumber of numbers found: " << j;
-    std::cout << "\nNumber found? " << found;
-    return j;
+    return numFound;
 }
 
-int atleast1SC (const std::string &pwd)
+bool atleast1SC (const std::string &pwd)
 {
-    bool scFound;
-    bool found;
+    bool scFound = false;
     int length = pwdLength(pwd.c_str());
     int j = 0;  //count the numbers of special character
-    int macroSC_len = sizeof(S_C)/sizeof(S_C[0]);
+    int macroSC_len = sizeof(SC)/sizeof(SC[0]);
     for(int i = 0;  i < length;  i++)
     {
         for( int k = 0; k < macroSC_len; k++)
         {
-            if(pwd.c_str()[i] == S_C[k])
+            if(pwd.c_str()[i] == SC[k])
             {
                 scFound = true;   //Found at least one special character
-                found = scFound;
                 j = j + 1;      //count how many special characters in the password
-                //std::cout << "\nSpecial character found in the loop? " << scFound;
             }
-            else
-                scFound = false;
         }
     }
     //std::cout << "\nNumber of special characters found: " << j;     //how many special characters?
-    //std::cout << "\nSpecial character found loop? " << found;
-    return j;
-}
-
-/* Check rockyou.txt that contains at least 8 characters */
-
-bool rockyouCheck(const std::string &pwd)
-{
-    /* rockyou.txt from KALI linux */
-    std::string word;
-    std::ifstream file("rockyou8.txt");
-    bool found = false;
-
-    if(file.is_open())
-    {
-        while(getline(file, word))
-        {
-            if(word == pwd.c_str())    //The password is in the list
-            {
-                found = true;
-                //std::cout <<"\nThis is the word: " << word;
-                //std::cout << "\nFound inside the loop: " << found;
-                break;
-            }
-            else
-                found = false;
-        }
-        file.close();
-    }
-    else
-        std::cout << "\n Error opening file, try again";
-    //std::cout << "\nFound outside the loop: " << found;
-
-    return found;
+    //std::cout << "\nSpecial character found loop? " << scFound;
+    return scFound;
 }
